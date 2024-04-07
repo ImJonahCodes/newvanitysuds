a7d1e9123618e8bcc699ba689115b4bd41518326
Jonah Codes <63764361+ImJonahCodes@users.noreply.github.com>
2024-04-07 06:18:41 +0000

    ghjkl

--- .vercel/output/functions/_render.func/workspaces/newvanitysuds/.vercel/output/_functions/chunks/manifest_QQQpFGLd.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_DWLn7jQV.mjs').then(n => n.m);
-
-export { page };
--- .vercel/output/functions/_render.func/workspaces/newvanitysuds/.vercel/output/_functions/chunks/scripts_CF4VFUXN.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/scripts_BGlXRuA5.mjs');
-
-export { page };
